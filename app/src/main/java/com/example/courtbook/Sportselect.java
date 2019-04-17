1fb744e30dafea88d917748fc22ef19866c80446
HarshaV23 <harsa007123@gmail.com>
2019-04-17 12:49:58 +0530

    Added Logout and functionality is perfect,item click in navigational view drawer working properly.

--- app/src/main/java/com/example/courtbook/Sportselect.java
+++ app/src/main/java/com/example/courtbook/Sportselect.java
@@ -3,6 +3,7 @@ package com.example.courtbook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,10 +11,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
@@ -23,11 +27,13 @@ import static com.example.courtbook.R.id.logout;
 
 public class Sportselect extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private RecyclerView recyclerView;
+    private static final int RC_SIGN_IN=0;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
    private DrawerLayout mdrawer;
     private ActionBarDrawerToggle abt;
-    NavigationView navigationView;
+   private    FirebaseAuth firebaseAuth;
+
 
 
     @Override
@@ -36,34 +42,48 @@ public class Sportselect extends AppCompatActivity implements NavigationView.OnN
         setContentView(R.layout.activity_sportselect);
 
 
-        navigationView=findViewById(R.id.navid);
-        navigationView.setNavigationItemSelectedListener(this);
+        NavigationView mnavigationview=(NavigationView) findViewById(R.id.navid);
+        //Listening to items clicked
+        if(mnavigationview!=null)
+        {
+            mnavigationview.setNavigationItemSelectedListener(this);
+            mnavigationview.bringToFront();
+        }
         mdrawer= findViewById(R.id.drawerid);
 
 
-
+        //Adding the menu icon to the nav drawer
         abt =new ActionBarDrawerToggle(this,mdrawer,R.string.open,R.string.close);
         mdrawer.addDrawerListener(abt);
         abt.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Firebase Auth using email and google providers
+        firebaseAuth=FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser()!=null){
+            //user already signed in
+            Log.d("Auth",firebaseAuth.getCurrentUser().getEmail());
+        }
+        else {
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(Arrays.asList(
+                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                            new AuthUI.IdpConfig.EmailBuilder().build()
+                            )
+                    )
 
-        FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
-        startActivityForResult(AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(Arrays.asList(
-                        new AuthUI.IdpConfig.GoogleBuilder().build(),
-                        new AuthUI.IdpConfig.EmailBuilder().build()
-                        )
-                )
+                    .build(), RC_SIGN_IN);
+        }
 
-                .build(),1 );
+
+        //Getting the items for the cards in the view.
         ArrayList<Getsport>getsports=new ArrayList<>();
         getsports.add(new Getsport(1 ,R.drawable.cricket,"Cricket"));
         getsports.add(new Getsport(2, R.drawable.badminton,"Badminton"));
         getsports.add(new Getsport(3,R.drawable.football,"Football"));
 
-
+        //setting the layout and adapter.
         recyclerView=findViewById(R.id.recyclerSport);
         layoutManager=new LinearLayoutManager(this);
 
@@ -84,17 +104,39 @@ public class Sportselect extends AppCompatActivity implements NavigationView.OnN
         return super.onOptionsItemSelected(item);
     }
 
-
+//Actions on the nav items clicked.
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-
-       if(menuItem.getItemId()==R.id.logout){
-           Toast.makeText(this,"g",Toast.LENGTH_SHORT).show();
+        int id=menuItem.getItemId();
+       if(id == R.id.logout){
+           AuthUI.getInstance()
+                   .signOut(this)
+                   .addOnCompleteListener(new OnCompleteListener<Void>() {
+                       @Override
+                       public void onComplete(@NonNull Task<Void> task) {
+                           Log.d("Auth","USER LOGGED OUT");
+                           finish();
+                       }
+                   });
         }
 
-        mdrawer.closeDrawer(GravityCompat.START);
-        return true;
+      return false;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+         if(requestCode==RC_SIGN_IN)
+         {
+             if(resultCode==RESULT_OK){
+                 //user logged in
+                Log.d("Auth",firebaseAuth.getCurrentUser().getEmail());
+             }
+             else{
+                 //User not authenticated
+                 Log.d("AUTH","NOT AUTHENTICATED");
+             }
+         }
     }
 }
 
